d0de81172c16679ae78a7aa4ca1486aa713c6a96
Arya Maddel <maddelarya@gmail.com>
2023-04-17 23:20:22 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(152, 37)
+        Label1.TabIndex = 0
+        Label1.Text = "Attendance"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(12, 112)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(101, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Enter name"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(134, 112)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(169, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(134, 150)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(169, 23)
+        TextBox2.TabIndex = 4
+        TextBox2.Text = " "
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(12, 150)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(116, 25)
+        Label3.TabIndex = 3
+        Label3.Text = "Enter Roll no:"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(330, 112)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(111, 124)
+        ListBox1.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(81, 247)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(174, 109)
+        Button1.TabIndex = 6
+        Button1.Text = "Present"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(330, 399)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(111, 39)
+        Button2.TabIndex = 7
+        Button2.Text = "Save Attendance"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(486, 450)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Controls.Add(TextBox2)
+        Controls.Add(Label3)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String = TextBox1.Text
+        Dim rollNumber As Integer = CInt(TextBox2.Text)
+
+        Dim student As New Student(name, rollNumber, "Present")
+
+        ' Add the student to the list of students
+        ListBox1.Items.Add(student)
+
+        ' Clear the name textbox for the next entry
+        TextBox1.Clear()
+        TextBox2.Clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Create a new save file dialog
+        Dim saveFileDialog1 As New SaveFileDialog()
+
+        ' Set the file filter to text files only
+        saveFileDialog1.Filter = "Text files (*.txt)|*.txt"
+
+        ' Show the save file dialog
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+            ' Open the file for writing
+            Dim fileWriter As New StreamWriter(saveFileDialog1.FileName)
+
+            ' Loop through the items in the listbox
+            For Each item In ListBox1.Items
+                ' Write each student's data to the file
+                fileWriter.WriteLine(item.ToString())
+            Next
+
+            ' Close the file
+            fileWriter.Close()
+
+            ' Show a message box to confirm the file was saved
+            MessageBox.Show("File saved successfully!")
+        End If
+    End Sub
+End Class
+
+Public Class Student
+    Public Property Name As String
+    Public Property RollNumber As Integer
+    Public Property AttendanceStatus As String
+
+    Public Sub New(name As String, rollnumber As Integer, attendanceStatus As String)
+        Me.Name = name
+        Me.RollNumber = rollnumber
+        Me.AttendanceStatus = attendanceStatus
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return RollNumber & " - " & Name & " - " & AttendanceStatus
+    End Function
+End Class
